c4f27659b808b78f58675a0daaaffb52b7a0717f
rnuyttens <66874841+rnuyttens@users.noreply.github.com>
2023-12-08 17:35:48 +0100

    Update allied_telesis_awplus.py

--- nornir_nautobot/plugins/tasks/dispatcher/allied_telesis_awplus.py
+++ nornir_nautobot/plugins/tasks/dispatcher/allied_telesis_awplus.py
@@ -1,1 +1,13 @@
+"""nornir dispatcher for Allied Telesis Awplus."""
 
+from nornir_nautobot.plugins.tasks.dispatcher.default import NetmikoDefault
+
+
+class NapalmCiscoIos(NapalmDefault):
+    """Collection of Napalm Nornir Tasks specific toAllied Telesis Awplus devices."""
+
+
+class NetmikoAlliedTelesisAwplus(NetmikoDefault):
+    """Collection of Netmiko Nornir Tasks specific to Allied Telesis Awplus devices."""
+
+    config_command = "show run"
